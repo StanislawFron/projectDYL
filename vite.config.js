6ebc38852efc41178689148567721472542e4fd5
sfron <stasiu.fron2003@gmail.com>
2025-08-10 17:44:47 +0200

    Revert front styles changes

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,6 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
         cors: true,
     },
 });
